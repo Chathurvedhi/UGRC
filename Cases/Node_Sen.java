3fe111147f6ce4e8b5e0f619e9a2a92d9961362b
Chathurvedhi <chathurvedhitalapaneni@gmail.com>
2023-12-05 12:17:22 +0530

    Context Sens Donw

--- Cases/Node_Sen.java
+++ Cases/Node_Sen.java
@@ -21,24 +21,40 @@ public class Node_Sen {
 
         DualList list = new DualList();         // O1; Stack = {(list, O1)}   
                 
+        // Var Set: {list}; Ref Set: {O1}
+        
         list.head1 = new Node(5);           // O2; Stack = {(list, O1)}; Heap = {(O1, head1, O2)} 
                 
+        // Var Set: {list}; Ref Set: {O1, O2, O2_data}
+        
         // Call 1:
         PrintList1(list);                       // Stack = {(list, O1)}; Heap = {(O1, head1, O2)}                
+        
+        // Var Set: {list}; Ref Set: {O1, O2, O2_data}
         
         // Call 2:
         AddList3(list);                         // Stack = {(list, O1)}; Heap = {(O1, head1, O2), (O1, head3, O3)}                                      
         
+        // Var Set: {list}; Ref Set: {O1, O3, O3_data, O2, O2_data}
+        
         // Call 3:
         PrintList1(list);                       // Stack = {(list, O1)}; Heap = {(O1, head1, O2), (O1, head3, O3)}         
         
+        // Var Set: {list}; Ref Set: {O1, O3, O3_data}
+        
         list.head2 = new Node(2);           // O4; Stack = {(list, O1)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}        
                 
+        // Var Set: {list}; Ref Set: {O1, O3, O3_data, O4, O4_data}
+        
         // Call 4:
         PrintList2(list);                       // Stack = {(list, O1)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)} 
                 
+        // Var Set: {list}; Ref Set: {O1, O3, O3_data}
+        
         Node currNode = list.head3;             // Stack = {(list, O1), (currNode, O3)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}       
         
+        // Var Set: {currNode}; Ref Set: {O3, O3_data}
+
         System.out.println(currNode.data);      // Stack = {(list, O1), (currNode, O3)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}
         
         // Var Set: {}; Ref Set: {}
@@ -69,12 +85,18 @@ We create mapping for the formal and actual parameters only for variables not re
     {   
         // Base Stack = {ll, O1}, Heap = {(O1, head1, O2)}
 
+        // Var Set: {ll}; Ref Set: {O1, O2, O2_data}
+
         Node currNode = ll.head1;           // Stack = {(ll, O1), (currNode, O2)}; Heap = {(O1, head1, O2)}    
+
+        // Var Set: {ll, currNode}; Ref Set: {O1, O2, O2_data}
 
         System.out.println(currNode.data);  // Stack = {(ll, O1), (currNode, O2)}; Heap = {(O1, head1, O2)}    
     
+        // Var Set: {ll}; Ref Set: {O1, O2, O2_data}
     }
 
+    Mapping : (list - ll)
 */
 
 /* Call 2:
@@ -82,12 +104,18 @@ We create mapping for the formal and actual parameters only for variables not re
     {
         // Base Stack = {ll, O1}, Heap = {(O1, head1, O2)}
 
+        // Var Set: {ll}; Ref Set: {O1, O2, O2_data}
+
         Node currNode = new Node(7);        // O3; Stack = {(ll, O1), (currNode, O3)}; Heap = {(O1, head1, O2)}
 
+        // Var Set: {ll, currNode}; Ref Set: {O1, O3, O3_data, O2, O2_data}
+        
         ll.head3 = currNode;                // Stack = {(ll, O1), (currNode, O3)}; Heap = {(O1, head1, O2), (O1, head3, O3)}
 
+        // Var Set: {ll}; Ref Set: {O1, O3, O3_data, O2, O2_data}
     }
 
+    Mapping : (list - ll)
 */
 
 /* Call 3:
@@ -95,12 +123,19 @@ We create mapping for the formal and actual parameters only for variables not re
     {   
         // Base Stack = {ll, O1}, Heap = {(O1, head1, O2), (O1, head3, O3)}
 
+        // Var Set: {ll}; Ref Set: {O1, O3, O3_data, O2, O2_data}
+        
         Node currNode = ll.head1;           // Stack = {(ll, O1), (currNode, O2)}; Heap = {(O1, head1, O2), (O1, head3, O3)}    
 
+        // Var Set: {ll, currNode}; Ref Set: {O1, O3, O3_data, O2, O2_data}
+        
         System.out.println(currNode.data);  // Stack = {(ll, O1), (currNode, O2)}; Heap = {(O1, head1, O2), (O1, head3, O3)}    
-    
+        // O2 dies here !!!
+
+        // Var Set: {ll}; Ref Set: {O1, O3, O3_data}
     }
 
+    Mapping : (list - ll)
 */
 
 /* Call 4:
@@ -108,12 +143,18 @@ We create mapping for the formal and actual parameters only for variables not re
     {
         // Base Stack = {ll, O1}, Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}
 
-        Node currNode = ll.head2;           // Stack = {(ll, O1), (currNode, O2)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}    
+        // Var Set: {ll}; Ref Set: {O1, O3, O3_data, O4, O4_data}
 
-        System.out.println(currNode.data);  // Stack = {(ll, O1), (currNode, O2)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}    
+        Node currNode = ll.head2;           // Stack = {(ll, O1), (currNode, O4)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}    
+
+        // Var Set: {ll, currNode}; Ref Set: {O1, O3, O3_data, O4, O4_data}
+
+        System.out.println(currNode.data);  // Stack = {(ll, O1), (currNode, O4)}; Heap = {(O1, head1, O2), (O1, head3, O3), (O1, head2, O4)}    
     
+        Var Set: {ll}; Ref Set: {O1, O3, O3_data}
     }
 
+    Mapping : (list - ll)
 */
 
 
